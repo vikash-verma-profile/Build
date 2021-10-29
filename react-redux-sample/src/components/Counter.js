7a8f8a09120b4a5a4766872fe8237e29d669ad00
vikash-verma-profile <er.vikashverma@yahoo.com>
2021-10-29 13:11:36 +0530

    first commit

--- react-redux-sample/src/components/Counter.js
+++ react-redux-sample/src/components/Counter.js
@@ -1,31 +1,78 @@
 import classes from './Counter.module.css';
 import { useSelector, connect, useDispatch } from 'react-redux';
+import { Component } from 'react';
 
 const Counter = () => {
 
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    dispatch({type: 'increment' });
+  }
+  
+  const increaseHandler = () => {
+    dispatch({type: 'increase',amount:10 });
   }
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   }
-
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
+// connect(counterReducerIncrement, mapDispatchtoprops)(Counter)
+// connect(counterReducerDecrement, mapDispatchtoprops)(Counter)
 
 export default Counter;
+
+
+// class Counter extends Component {
+//   incrementHandler = () => {
+//     this.props.increment();
+//   }
+//   decrementHandler = () => {
+//     this.props.decrement();
+//   }
+//   toggleCounterHandler() {
+
+//   }
+
+//   render() {
+//     return (<main className={classes.counter}>
+//       <h1>Redux Counter</h1>
+//       <div className={classes.value}>{this.props.counter}</div>
+//       <div>
+//         <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//         <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//       </div>
+//       <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
+//     </main>);
+//   }
+// }
+
+// const mapStatetoprops = state => {
+//   return { counter: state.counter }
+// }
+
+// const mapDispatchtoprops = dispatch => {
+//   return {
+//     increment: () => dispatch({ type: 'increment' }),
+//     decrement: () => dispatch({ type: 'decrement' })
+//   }
+// }
+// export default connect(mapStatetoprops, mapDispatchtoprops)(Counter);
--- react-redux-sample/src/store/index.js
+++ react-redux-sample/src/store/index.js
@@ -1,20 +1,67 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+
+
+// const counterReducerIncrement = (state = { counter: 0 }, action) => {
+//     if (action.type === "increment") {
+//         return {
+//             counter: state.counter + action.amount,
+//         };
+//     }
+
+//     // if (action.type === "decrement") {
+//     //     return {
+//     //         counter: state.counter - 1,
+//     //     };
+//     // }
+//     return state;
+// }
+
+
+// const counterReducerDecrement = (state = { counter: 0 }, action) => {
+
+//     if (action.type === "decrement") {
+//         return {
+//             counter: state.counter - 1,
+//         };
+//     }
+//     return state;
+// }
+
+// const rootreducer=combineReducers({
+//     increment:counterReducerIncrement,
+//     decrement:counterReducerDecrement
+// })
+
+const intialState = { counter: 0, showCounter: true };
+const counterReducer = (state = intialState, action) => {
     if (action.type === "increment") {
         return {
             counter: state.counter + 1,
+            showCounter: state.showCounter,
         };
     }
-
+    if (action.type === "increase") {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter,
+        };
+    }
     if (action.type === "decrement") {
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter,
+        };
+    }
+
+    if (action.type === "toggle") {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         };
     }
     return state;
 }
-
-const store=createStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
